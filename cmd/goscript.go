0d7c2c95d6042c6acea6cb406f9126342257d42a
John Mac <charles.beck@sent.com>
2010-12-07 15:44:10 +0000

    Added flag to beused into shared filesystem

--- cmd/goscript.go
+++ cmd/goscript.go
@@ -11,6 +11,7 @@ package main
 
 import (
 	"exec"
+	"flag"
 	"fmt"
 	"os"
 	"path"
@@ -18,17 +19,110 @@ import (
 	"strings"
 )
 
-const EXIT_CODE = 1
+// Error exit status
+const ERROR = 1
 
+// Environment variables passed at running a command
 var ENVIRON []string
 
+
+// === Flags
+// ===
+
+var fShared = flag.Bool("shared", false,
+	"whether the script is used on a mixed network of machines or   "+
+	+"systems from a shared filesystem")
+
+func usage() {
+	flag.PrintDefaults()
+	os.Exit(ERROR)
+}
+// ===
+
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		fmt.Fprintf(os.Stderr,
+			" Usage: Insert \"#!/usr/bin/env goscript\" in the head of a Go script\n")
+		usage()
+	}
+
+	scriptPath := flag.Args()[0] // Relative path
+	scriptDir, scriptName := path.Split(scriptPath)
+	binaryPath := ""
+
+	if !*fShared {
+		binaryPath = path.Join(scriptDir, ".go", scriptName)
+	} else {
+		subDir := runtime.GOOS + "_" + runtime.GOARCH
+		binaryPath = path.Join(scriptDir, ".go", subDir, scriptName)
+	}
+
+	// === Run the executable, if exist and it has not been modified
+	if _, err := os.Stat(binaryPath); err == nil {
+		scriptMtime := getTime(scriptPath)
+		binaryMtime := getTime(binaryPath)
+
+		if scriptMtime == binaryMtime {
+			goto _run
+		}
+	}
+
+	// === Check script extension
+	if path.Ext(scriptPath) != ".g" {
+		fmt.Fprintf(os.Stderr, "Wrong extension! It has to be \".g\"\n")
+		os.Exit(ERROR)
+	}
+
+	// === Compile and link
+	scriptMtime := getTime(scriptPath)
+	comment(scriptPath, true)
+	compiler, linker, archExt := toolchain()
+
+	ENVIRON = os.Environ()
+	objectFile := "_go_." + archExt
+
+	cmdArgs := []string{path.Base(compiler), "-o", objectFile, scriptName}
+	exitCode := run(compiler, cmdArgs, scriptDir)
+	comment(scriptPath, false)
+	if exitCode != 0 {
+		os.Exit(exitCode)
+	}
+
+	cmdArgs = []string{path.Base(linker), "-o", scriptName, objectFile}
+	if exitCode = run(linker, cmdArgs, scriptDir); exitCode != 0 {
+		os.Exit(exitCode)
+	}
+
+	// === Cleaning
+	// Set mtime of executable just like the source file
+	setTime(scriptPath, scriptMtime)
+	setTime(binaryPath, scriptMtime)
+
+	if err := os.Remove(path.Join(scriptDir, objectFile)); err != nil {
+		fmt.Fprintf(os.Stderr, "Could not remove: %s\n", err)
+		os.Exit(ERROR)
+	}
+	// ===
+
+_run:
+	exitCode = run(binaryPath, []string{scriptName}, "")
+	os.Exit(exitCode)
+}
+
+
+// === Utility
+// ===
 
 // Base to access to "mtime" of given file.
 func _time(filename string, mtime int64) int64 {
 	info, err := os.Stat(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not access: %s\n", err)
-		os.Exit(EXIT_CODE)
+		os.Exit(ERROR)
 	}
 	
 	if mtime != 0 {
@@ -68,7 +162,7 @@ func comment(filename string, ok bool) {
 
 _error:
 	fmt.Fprintf(os.Stderr, "Could not write: %s\n", err)
-	os.Exit(EXIT_CODE)
+	os.Exit(ERROR)
 }
 
 // Executes a command and returns its exit code.
@@ -79,7 +173,7 @@ func run(cmd string, args []string, dir string) int {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not execute: \"%s\"\n",
 			strings.Join(args, " "))
-		os.Exit(EXIT_CODE)
+		os.Exit(ERROR)
 	}
 
 	// Wait for command completion
@@ -87,7 +181,7 @@ func run(cmd string, args []string, dir string) int {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not wait for: \"%s\"\n",
 			strings.Join(args, " "))
-		os.Exit(EXIT_CODE)
+		os.Exit(ERROR)
 	}
 
 	return message.ExitStatus()
@@ -108,7 +202,7 @@ func toolchain() (compiler, linker, archExt string) {
 		if goroot == "" {
 			fmt.Fprintf(os.Stderr, "Environment variable GOROOT neither"+
 				" GOROOT_FINAL has been set\n")
-			os.Exit(EXIT_CODE)
+			os.Exit(ERROR)
 		}
 	}
 
@@ -126,77 +220,11 @@ func toolchain() (compiler, linker, archExt string) {
 	archExt, ok := arch_ext[goarch]
 	if !ok {
 		fmt.Fprintf(os.Stderr, "Unknown GOARCH: %s\n", goarch)
-		os.Exit(EXIT_CODE)
+		os.Exit(ERROR)
 	}
 
 	compiler = path.Join(gobin, archExt+"g")
 	linker = path.Join(gobin, archExt+"l")
 	return
-}
-
-
-func main() {
-	args := os.Args
-	if len(args) != 2 {
-		fmt.Println(`Usage: Insert "#!/usr/bin/env goscript" in your Go script`)
-		return
-	}
-
-	scriptPath := args[1] // Relative path
-	scriptDir, scriptFile := path.Split(scriptPath)
-	// The executable is an hidden file.
-	binaryFile := "." + scriptFile
-	binaryPath := path.Join(scriptDir, binaryFile)
-
-	// === Run the executable, if exist and it has not been modified
-	if _, err := os.Stat(binaryPath); err == nil {
-		scriptMtime := getTime(scriptPath)
-		binaryMtime := getTime(binaryPath)
-
-		if scriptMtime == binaryMtime {
-			goto _run
-		}
-	}
-
-	// === Check script extension
-	if path.Ext(scriptPath) != ".g" {
-		fmt.Fprintf(os.Stderr, "Wrong extension! It has to be \".g\"\n")
-		os.Exit(EXIT_CODE)
-	}
-
-	// === Compile and link
-	scriptMtime := getTime(scriptPath)
-	comment(scriptPath, true)
-	compiler, linker, archExt := toolchain()
-
-	ENVIRON = os.Environ()
-	objectFile := "_go_." + archExt
-
-	cmdArgs := []string{path.Base(compiler), "-o", objectFile, scriptFile}
-	exitCode := run(compiler, cmdArgs, scriptDir)
-	comment(scriptPath, false)
-	if exitCode != 0 {
-		os.Exit(exitCode)
-	}
-
-	cmdArgs = []string{path.Base(linker), "-o", binaryFile, objectFile}
-	if exitCode = run(linker, cmdArgs, scriptDir); exitCode != 0 {
-		os.Exit(exitCode)
-	}
-
-	// === Cleaning
-	// Set mtime of executable just like the source file
-	setTime(scriptPath, scriptMtime)
-	setTime(binaryPath, scriptMtime)
-
-	if err := os.Remove(path.Join(scriptDir, objectFile)); err != nil {
-		fmt.Fprintf(os.Stderr, "Could not remove: %s\n", err)
-		os.Exit(EXIT_CODE)
-	}
-	// ===
-
-_run:
-	exitCode = run(binaryPath, []string{binaryFile}, "")
-	os.Exit(exitCode)
 }
 
